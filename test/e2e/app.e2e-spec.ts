9279d7c3a7d008d70c1173e8a9b0266fbb462595
Dmitry Khorev <me@dkhorev.pro>
2021-11-14 18:04:12 +0300

    updated tests

--- test/e2e/app.e2e-spec.ts
+++ test/e2e/app.e2e-spec.ts
@@ -15,10 +15,11 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/ (GET) should return code 200', () => {
+    return request(app.getHttpServer()).get('/').expect(200);
+  });
+
+  it('/ (GET) should return empty body', () => {
+    return request(app.getHttpServer()).get('/').expect('');
   });
 });
--- test/unit/app.controller.spec.ts
+++ test/unit/app.controller.spec.ts
@@ -4,6 +4,7 @@ import { AppService } from '../../src/app.service';
 
 describe('AppController', () => {
   let appController: AppController;
+  let appService: AppService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -12,11 +13,20 @@ describe('AppController', () => {
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppService);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return empty body', () => {
+      expect(appController.new()).toBe('');
+    });
+
+    it('should call AppService.new() once', () => {
+      jest.spyOn(appService, 'new').mockImplementation(() => '');
+
+      appController.new();
+
+      expect(appService.new).toHaveBeenCalledTimes(1);
     });
   });
 });
