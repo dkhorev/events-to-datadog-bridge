7e4e55b56ddb18f8ba1fa57903baac49d78d79c8
Dmitry Khorev <me@dkhorev.pro>
2021-11-15 08:27:42 +0300

    switch to fastify and add fastify-helmet

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { fastifyHelmet } from 'fastify-helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true, bodyLimit: 10000 }),
+  );
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 3000);
+
+  await app.register(fastifyHelmet);
+
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 
 bootstrap();
