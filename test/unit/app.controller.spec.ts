98ef98c93e7b8e36426677abbe1f1504463b20ec
Dmitry Khorev <me@dkhorev.pro>
2021-11-19 20:19:30 +0300

    improving test coverage

--- test/unit/app.controller.spec.ts
+++ test/unit/app.controller.spec.ts
@@ -1,28 +1,15 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { AppController } from '../../src/app.controller';
 import { AppService } from '../../src/app.service';
-import { MetricsModule } from '../../src/metrics/metrics.module';
-import { ConfigModule } from '@nestjs/config';
-import { ThrottlerModule } from '@nestjs/throttler';
+import { AppModule } from '../../src/app.module';
 
 describe('AppController', () => {
   let appController: AppController;
   let appService: AppService;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      imports: [
-        MetricsModule,
-        ConfigModule.forRoot({
-          isGlobal: true,
-        }),
-        ThrottlerModule.forRoot({
-          ttl: 60,
-          limit: 60,
-        }),
-      ],
-      controllers: [AppController],
-      providers: [AppService],
+    const app = await Test.createTestingModule({
+      imports: [AppModule],
     }).compile();
 
     appController = app.get<AppController>(AppController);
